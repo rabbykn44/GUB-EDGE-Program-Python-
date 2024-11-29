e3417c7a81fd12fbc983ae8c872c43f51b43173f
Md.Rabby Khan <125567413+rabbykn44@users.noreply.github.com>
2024-11-29 23:10:20 +0600

    Add files via upload

---
+++ Class_1_213902037_Md.Rabby Khan_16-10-24_Task.py
@@ -0,0 +1,11 @@
+name = input("Please enter your name: ")
+print(f"How are you, Mr. {name}")
+
+age = input("Enter your age: ")
+print(f"Your age is: {age}")
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+sum_result = a + b
+
+print(f"The summation is: {sum_result}")
